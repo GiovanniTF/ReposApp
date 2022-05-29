1ee22b3de8fac76af92650c360f693970cf01c4c
giovannitf <Gi030201>
2022-05-29 18:42:23 -0300

    Cria repositorio git

---
+++ app/src/main/java/br/com/giovanni/reposapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.giovanni.reposapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
